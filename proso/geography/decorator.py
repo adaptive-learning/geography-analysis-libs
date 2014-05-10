0d8e382d6d04d058413e05446d0a16eb8a5b1ffb
Jan Papousek <jan.papousek@gmail.com>
2014-05-10 08:45:02 +0200

    add last_in_session and rolling_success to the decorator

--- proso/geography/decorator.py
+++ proso/geography/decorator.py
@@ -26,6 +26,74 @@ def session_number(answers, delta_in_seconds=1800, override=False):
         sort())
 
 
+def last_in_session(answers, override=False):
+    '''
+    Assign the boolean marker to each answer whether the answer is the last in
+    the given session.
+
+    Args:
+        answers (pandas.DataFrame):
+            data frame containing answer data
+        override (bool, optional, default False):
+            if False and the data contains 'last_in_session' column already, the
+            decoration will be skipped.
+    Returns:
+        pandas.DataFrame: data frame containing answer data
+    '''
+    if not override and 'last_in_session' in answers:
+        return answers
+    if 'session_number' in answers:
+        data = answers
+    else:
+        data = session_number(answers)
+    return (data.
+        groupby('user').
+        apply(_last_in_session_for_user).
+        sort())
+
+
+def rolling_success(answers, window_length=10, override=False):
+    '''
+    Assign the rolling success to each answer.
+
+    Args:
+        answers (pandas.DataFrame):
+            data frame containing answer data
+        window_length (int, optional):
+            the rolling success is computed for only a few last answers - window,
+            this parameter sets the size of this window
+        override (bool, optional, default False):
+            if False and the data contains 'rolling_success' column already, the
+            decoration will be skipped.
+    Returns:
+        pandas.DataFrame: data frame containing answer data
+    '''
+    if not override and 'rolling_success' in answers:
+        return answers
+    if 'last_in_session' in answers:
+        data = answers
+    else:
+        data = last_in_session(answers)
+    return (data.
+        groupby(['user', 'session_number']).
+        apply(lambda x: _rolling_succcess_for_user_session(x, window_length)).
+        sort())
+
+
+def _last_in_session_for_user(group):
+    data = group.sort(['session_number', 'id'])
+    data['last_in_session'] = data['session_number'] != data.shift(-1)['session_number']
+    return data
+
+
+def _rolling_succcess_for_user_session(group, window_length):
+    group['rolling_success'] = pd.rolling_apply(
+        group['place_asked'] == group['place_answered'],
+        window_length,
+        lambda x: sum(x) / float(window_length))
+    return group
+
+
 def _session_number_for_user(group, delta_in_seconds):
     session_duration = np.timedelta64(delta_in_seconds, 's')
     group['session_number'] = (
