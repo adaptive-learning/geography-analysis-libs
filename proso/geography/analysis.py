5a139014903a1b5382ad816a9701c7c3944b6874
Jan Papousek <jan.papousek@gmail.com>
2014-06-03 08:09:01 +0200

    seperate funcionality for scripts

---
+++ proso/geography/analysis.py
@@ -0,0 +1,139 @@
+from argparse import ArgumentParser
+from os import path, makedirs
+import proso.geography.answers as answer
+import proso.geography.decorator as decorator
+import proso.geography.difficulty
+
+
+def parser_init():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-a',
+        '--answers',
+        metavar='FILE',
+        required=True,
+        help='path to the CSV with answers')
+    parser.add_argument(
+        '--options',
+        metavar='FILE',
+        help='path to the CSV with answer options')
+    parser.add_argument(
+        '--ab-values',
+        metavar='FILE',
+        dest='ab_values',
+        help='path to the CSV with ab values')
+    parser.add_argument(
+        '--answer-ab-values',
+        metavar='FILE',
+        dest='answer_ab_values',
+        help='path to the CSV with answer ab values')
+    parser.add_argument(
+        '-d',
+        '--destination',
+        metavar='DIR',
+        required=True,
+        help='path to the directory where the created data will be saved')
+    parser.add_argument(
+        '-o',
+        '--output',
+        metavar='EXT',
+        dest='output',
+        default='svg',
+        help='extension for the output fles')
+    parser.add_argument(
+        '--optimize',
+        default=True,
+        metavar='BOOL',
+        help='enables decorator optimization')
+    parser.add_argument(
+        '--drop-classrooms',
+        type=bool,
+        default=True,
+        dest='drop_classrooms',
+        help='drop users having some of the first answer from classroom')
+    return parser
+
+
+def parser_group(parser, groups):
+    parser.add_argument(
+        '--group',
+        choices=groups,
+        help='generate only a limited set of plots')
+    return parser
+
+
+def decorator_optimization(answers):
+    decorated = decorator.rolling_success(
+        decorator.last_in_session(
+            decorator.session_number(answers)))
+    return decorated
+
+
+def load_answers(args):
+    if not path.exists(args.destination):
+        makedirs(args.destination)
+    if path.exists(args.destination + '/geography.answer.csv'):
+        return answer.from_csv(args.destination + '/geography.answer.csv')
+    else:
+        data = answer.from_csv(
+            answer_csv=args.answers,
+            answer_options_csv=args.options,
+            answer_ab_values_csv=args.answer_ab_values,
+            ab_value_csv=args.ab_values)
+        if args.drop_classrooms:
+            classroom_users = [
+                user
+                for ip, users in (
+                    data.sort('id').drop_duplicates('user').
+                    groupby('ip_address').
+                    apply(lambda x: x['user'].unique()).
+                    to_dict().
+                    items())
+                for user in users
+                if len(users) > 5
+            ]
+            data = data[~data['user'].isin(classroom_users)]
+        if args.optimize:
+            data = decorator_optimization(data)
+            data.to_csv(args.destination + '/geography.answer.csv')
+        return data
+
+
+def load_difficulty(args, data):
+    if not path.exists(args.destination):
+        makedirs(args.destination)
+    if not path.exists(args.destination + '/difficulty.csv'):
+        difficulty = proso.geography.difficulty.prepare_difficulty(data)
+        proso.geography.difficulty.difficulty_to_csv(
+            difficulty, args.destination + '/difficulty.csv')
+    else:
+        difficulty = proso.geography.difficulty.csv_to_difficulty(
+            args.destination + '/difficulty.csv')
+    return difficulty
+
+
+def load_prior_skill(args, data, difficulty):
+    if not path.exists(args.destination):
+        makedirs(args.destination)
+    if not path.exists(args.destination + '/prior_skill.csv'):
+        prior_skill = proso.geography.user.prior_skill(data, difficulty)
+        proso.geography.user.prior_skill_to_csv(
+            prior_skill, args.destination + '/prior_skill.csv')
+    else:
+        prior_skill = proso.geography.user.csv_to_prior_skill(
+            args.destination + '/prior_skill.csv')
+    return prior_skill
+
+
+def savefig(args, figure, name):
+    if not path.exists(args.destination):
+        makedirs(args.destination)
+    figure.save(args.destination + '/' + name + '.' + args.output)
+
+
+def is_group(args, group):
+    return not args.group or args.group == group
+
+
+def is_any_group(args, groups):
+    return not args.group or args.group in groups
--- scripts/overview.py
+++ scripts/overview.py
@@ -1,94 +1,19 @@
-from argparse import ArgumentParser
-from os import path, makedirs
 import matplotlib.pyplot as plt
-import proso.geography.answers as answer
-import proso.geography.decorator as decorator
 import proso.geography.graph as graph
-import proso.geography.difficulty
 import proso.geography.user as user
-
-
-def get_parser():
-    parser = ArgumentParser()
-    parser.add_argument(
-        '-a',
-        '--answers',
-        metavar='FILE',
-        required=True,
-        help='path to the CSV with answers')
-    parser.add_argument(
-        '--options',
-        metavar='FILE',
-        help='path to the CSV with answer options')
-    parser.add_argument(
-        '-d',
-        '--destination',
-        metavar='DIR',
-        required=True,
-        help='path to the directory where the created data will be saved')
-    parser.add_argument(
-        '-o',
-        '--output',
-        metavar='EXT',
-        dest='output',
-        default='svg',
-        help='extension for the output fles')
-    parser.add_argument(
-        '--decorator',
-        default=True,
-        metavar='BOOL',
-        help='enables decorator optimization')
-    parser.add_argument(
-        '--group',
-        choices=['time', 'session', 'recommendation', 'knowledge'],
-        help='generate only a limited set of plots')
-    return parser
-
-
-def decorator_optimization(args, answers):
-    decorated = decorator.rolling_success(
-        decorator.last_in_session(
-            decorator.session_number(answers)))
-    decorated.to_csv(args.answers, index=False)
-    return decorated
-
-
-def decorator_options(args, answers):
-    if 'options' in answers:
-        return answers
-    if not args.options:
-        raise Exception('you have to define CSV with answer options')
-    return answer.options_from_csv(
-        answers=answers,
-        answer_options_csv=args.options)
+import proso.geography.analysis as analysis
 
 
 def main():
-    args = get_parser().parse_args()
-    if not path.exists(args.destination):
-        makedirs(args.destination)
-    data = answer.from_csv(args.answers)
-    data = decorator_options(args, data)
-    if args.decorator:
-        data = decorator_optimization(args, data)
-
-    if not path.exists(args.destination + '/difficulty.csv'):
-        difficulty = proso.geography.difficulty.prepare_difficulty(data)
-        proso.geography.difficulty.difficulty_to_csv(
-            difficulty, args.destination + '/difficulty.csv')
-    else:
-        difficulty = proso.geography.difficulty.csv_to_difficulty(
-            args.destination + '/difficulty.csv')
-
-    if not path.exists(args.destination + '/prior_skill.csv'):
-        prior_skill = proso.geography.user.prior_skill(data, difficulty)
-        proso.geography.user.prior_skill_to_csv(
-            prior_skill, args.destination + '/prior_skill.csv')
-    else:
-        prior_skill = proso.geography.user.csv_to_prior_skill(
-            args.destination + '/prior_skill.csv')
-
-    if not args.group or args.group == 'time':
+    parser = analysis.parser_init()
+    parser = analysis.parser_group(parser, ['time', 'session', 'recommendation', 'knowledge'])
+    args = parser.parse_args()
+    data = analysis.load_answers(args)
+    if analysis.is_any_group(args, ['recommendation', 'knowledge']):
+        difficulty = analysis.load_difficulty(args, data)
+    if analysis.is_any_group(args, ['recommendation']):
+        prior_skill = analysis.load_prior_skill(args, data, difficulty)
+    if analysis.is_group(args, 'time'):
         fig = plt.figure()
         graph.plot_answers_per_day(fig, data)
         fig.suptitle('Average number of answers per user')
@@ -96,7 +21,7 @@ def main():
         fig = plt.figure()
         graph.plot_success_per_week(fig, data)
         fig.savefig(args.destination + '/success_per_week.' + args.output)
-    if not args.group or args.group == 'session':
+    if analysis.is_group(args, 'session'):
         fig = plt.figure()
         graph.plot_session_length(fig, data)
         fig.suptitle('Session length')
@@ -104,11 +29,11 @@ def main():
         fig = plt.figure()
         graph.plot_session_success(fig, data)
         fig.savefig(args.destination + '/session_success.' + args.output)
-    if not args.group or args.group == 'recommendation':
+    if analysis.is_group(args, 'recommendation'):
         fig = plt.figure()
         graph.plot_stay_on_rolling_success(fig, data, prior_skill)
         fig.savefig(args.destination + '/stay_on_success.' + args.output)
-    if not args.group or args.group == 'knowledge':
+    if analysis.is_group(args, 'knowledge'):
         fig = plt.figure()
         graph.plot_session_prior_skill(fig, data, difficulty)
         fig.savefig(args.destination + '/session_prior_skill.' + args.output)
