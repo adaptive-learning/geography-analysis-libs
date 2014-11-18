94a02b062fe469d583cab3cc028871206fe0cac7
Jan Papousek <jan.papousek@gmail.com>
2014-11-18 08:07:44 +0100

    improve interactive script

--- interactive.py
+++ interactive.py
@@ -6,6 +6,8 @@ import proso.geography.overtime as overtime
 import proso.geography.session as session
 import proso.geography.success as success
 import proso.geography.user as user
+import proso.geography.difficulty as difficulty
+import proso.geography.dfutil as dfutil
 
 DEST_DIR = './dest-interactive'
 DATA_DIR = './data'
@@ -15,10 +17,19 @@ def reset_dest():
         removedirs(DEST_DIR)
     makedirs(DEST)
 
-data = answers.from_csv(
-    answer_csv=DATA_DIR + '/geography.answer.csv',
-    answer_options_csv=DATA_DIR + '/geography.answer_options.csv',
-    answer_ab_values_csv=DATA_DIR + '/geography.answer_ab_values.csv',
-    ab_value_csv=DATA_DIR + '/geography.ab_value.csv')
+def load_data():
+    filename = DEST_DIR + '/geography.answer.csv'
+    if path.exists(filename):
+        return answers.from_csv(filename)
+    else:
+        return answers.from_csv(
+            answer_csv=DATA_DIR + '/geography.answer.csv',
+            answer_options_csv=DATA_DIR + '/geography.answer_options.csv',
+            answer_ab_values_csv=DATA_DIR + '/geography.answer_ab_values.csv',
+            ab_value_csv=DATA_DIR + '/geography.ab_value.csv')
 
+#data = load_data()
+#data = data[data['place_map_code'].isin(['europe', 'world'])]
+
+#difficulty.prepare_difficulty(data)
 
