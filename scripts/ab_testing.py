eb0e21c80202c95e4a1a3abfc35aacf8167fe3f4
Jan Papousek <jan.papousek@gmail.com>
2014-12-04 14:23:48 +0100

    improve script for A/B testing analysis

--- scripts/ab_testing.py
+++ scripts/ab_testing.py
@@ -20,18 +20,117 @@ def load_parser():
         type=str,
         required=True,
         help='prefixes of A/B values which are used for analysis')
+    parser.add_argument(
+        '--split-maps',
+        nargs='+',
+        type=str,
+        dest='split_maps')
+    parser.add_argument(
+        '--filter-abvalue',
+        nargs='+',
+        type=str,
+        dest='filter_abvalue')
     return parser
 
 
 def load_answers_to_ab_testing(args):
-    filename = 'geography.answer.ab_testing_' + '__'.join(args.interested_prefixes) + '__' + analysis.data_hash(args)
+    filename = 'geography.answer.ab_testing_' + '__'.join(args.interested_prefixes) + '_f_' + '__'.join(args.filter_abvalue if args.filter_abvalue else []) + '__' + analysis.data_hash(args)
     data = analysis.read_cache(args, filename, csv_parser=answer.from_csv)
     if data is not None:
         return data
     data, _ = analysis.load_answers(args, all_needed=False)
     data = abtesting.prepare_data(data, args.interested_prefixes)
+    if args.filter_abvalue:
+        data = answer.apply_filter(data, lambda d: all(map(lambda g: g in d['ab_values'], args.filter_abvalue)))
     analysis.write_cache(args, data, filename)
     return data
+
+
+def map_graphs(args, data, mapping, prefix, filename_prefix, group_column):
+    filename_prefix += str(group_column) + "_"
+    if analysis.is_group(args, 'motivation'):
+        fig = plt.figure()
+        graph.boxplot_answers_per_user(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: number of answers per user')
+        analysis.savefig(args, fig, filename_prefix + 'answers_per_user_boxplot', prefix=prefix)
+
+        fig = plt.figure()
+        graph.hist_answers_per_user(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: number of answers per user')
+        analysis.savefig(args, fig, filename_prefix + 'answers_per_user_hist', prefix=prefix)
+
+        fig = plt.figure()
+        graph.boxplot_maps_per_user(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: number of maps per user')
+        analysis.savefig(args, fig, filename_prefix + 'maps_per_user_boxplot', prefix=prefix)
+
+        fig = plt.figure()
+        graph.hist_maps_per_user(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: number of maps per user')
+        analysis.savefig(args, fig, filename_prefix + 'maps_per_user_hist', prefix=prefix)
+
+        fig = plt.figure()
+        data = decorator.session_number(data)
+        graph.boxplot_answers_per_user(fig,
+            data[data['session_number'] == 0], group_column, mapping)
+        fig.suptitle('AB testing: number of answers per user (only the first session)')
+        analysis.savefig(args, fig, filename_prefix + 'answers_per_user_session_0_boxplot', prefix=prefix)
+
+        fig = plt.figure()
+        graph.hist_answers_per_user(fig,
+            data[data['session_number'] == 0], group_column, mapping)
+        fig.suptitle('AB testing: number of answers per user (only the first session)')
+        analysis.savefig(args, fig, filename_prefix + 'answers_per_user_session_0_hist', prefix=prefix)
+
+        fig = plt.figure()
+        graph.boxplot_success_per_user(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: mean success rate')
+        analysis.savefig(args, fig, filename_prefix + 'success_per_user', prefix=prefix)
+        print "Group [motivation] processed"
+    else:
+        print "Group [motivation] skipped"
+
+    if analysis.is_group(args, 'progress') and (mapping is None or all(map(lambda x: not isinstance(x, str) or x.isdigit(), mapping.values()))):
+        fig = plt.figure()
+        graph.plot_user_ratio(fig, data, group_column, mapping, session_numbers=[1, 2])
+        fig.suptitle('AB testing: Users with at least the given number of sessions')
+        analysis.savefig(args, fig, filename_prefix + 'users_with_n_sessions', prefix=prefix)
+
+        fig = plt.figure()
+        graph.plot_user_ratio(fig, data, group_column, mapping, answer_numbers_min=[20, 30, 50])
+        fig.suptitle('AB testing: Users with at least the given number of answers')
+        analysis.savefig(args, fig, filename_prefix + 'users_with_n_answers', prefix=prefix)
+        print "Group [progress] processed"
+    else:
+        print "Group [progress] skipped"
+
+    if analysis.is_group(args, 'difference'):
+        fig = plt.figure()
+        graph.boxplot_number_of_options(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: Number of options')
+        analysis.savefig(args, fig, filename_prefix + 'number_of_options', prefix=prefix)
+
+        fig = plt.figure()
+        graph.boxplot_time_gap(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: Average Time Gap per User')
+        analysis.savefig(args, fig, filename_prefix + 'time_gap', prefix=prefix)
+
+        fig = plt.figure()
+        graph.hist_answers_per_place_user(fig, data, group_column, mapping)
+        fig.suptitle('AB testing: Number of Answer per Place and User')
+        analysis.savefig(args, fig, filename_prefix + 'answers_per_place_user', prefix=prefix)
+        print "Group [difference] processed"
+    else:
+        print "Group [difference] skipped"
+
+    if analysis.is_group(args, 'text'):
+        with open(analysis.get_destination(args, prefix) + '/' + filename_prefix + 'output.txt', 'w') as f:
+            textstats.answers_per_user(f, data, group_column, mapping)
+            textstats.answers_per_user_pvalues(f, data, group_column, mapping)
+            textstats.user_ratio(f, data, group_column, mapping)
+        print "Group [text] processed"
+    else:
+        print "Group [text] skipped"
 
 
 def main():
@@ -42,94 +141,32 @@ def main():
 
     data = load_answers_to_ab_testing(args)
     data, mapping = decorator.ab_group(data, args.interested_prefixes)
+    if args.interested_prefixes == ['recommendation_target_prob_adjustment_']:
+        mapping['ab_group'] = 'Is target probability adjustment enabled?'
+    elif args.interested_prefixes == ['recommendation_target_prob_']:
+        mapping['ab_group'] = 'Target probability'
+    elif sorted(args.interested_prefixes) == ['recommendation_by_', 'recommendation_options_']:
+        revert_mapping = dict(map(lambda (x, y): (y, x), mapping.items()))
+        mapping['ab_group'] = 'Recommendation algorithm'
+        mapping[revert_mapping['additive_function__naive']] = 'score, naive'
+        mapping[revert_mapping['additive_function__random']] = 'score, random'
+        mapping[revert_mapping['random__naive']] = 'random, naive'
+        mapping[revert_mapping['random__random']] = 'random, random'
 
     if len(data) == 0:
         print "There are no answers to analyze"
         return
-
-    if analysis.is_group(args, 'motivation'):
-        fig = plt.figure()
-        graph.boxplot_answers_per_user(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: number of answers per user')
-        analysis.savefig(args, fig, 'answers_per_user_boxplot', prefix=prefix)
-
-        fig = plt.figure()
-        graph.hist_answers_per_user(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: number of answers per user')
-        analysis.savefig(args, fig, 'answers_per_user_hist', prefix=prefix)
-
-        fig = plt.figure()
-        graph.boxplot_maps_per_user(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: number of maps per user')
-        analysis.savefig(args, fig, 'maps_per_user_boxplot', prefix=prefix)
-
-        fig = plt.figure()
-        graph.hist_maps_per_user(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: number of maps per user')
-        analysis.savefig(args, fig, 'maps_per_user_hist', prefix=prefix)
-
-        fig = plt.figure()
-        data = decorator.session_number(data)
-        graph.boxplot_answers_per_user(fig,
-            data[data['session_number'] == 0], 'ab_group', mapping)
-        fig.suptitle('AB testing: number of answers per user (only the first session)')
-        analysis.savefig(args, fig, 'answers_per_user_session_0_boxplot', prefix=prefix)
-
-        fig = plt.figure()
-        graph.hist_answers_per_user(fig,
-            data[data['session_number'] == 0], 'ab_group', mapping)
-        fig.suptitle('AB testing: number of answers per user (only the first session)')
-        analysis.savefig(args, fig, 'answers_per_user_session_0_hist', prefix=prefix)
-
-        fig = plt.figure()
-        graph.boxplot_success_per_user(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: mean success rate')
-        analysis.savefig(args, fig, 'success_per_user', prefix=prefix)
-        print "Group [motivation] processed"
+    if args.split_maps:
+        for map_name, map_data in data.groupby(args.split_maps):
+            map_prefix = (map_name if isinstance(map_name, str) else '_'.join(map_name)) + '__'
+            print "# Processing AB group"
+            map_graphs(
+                args, map_data, mapping, prefix,
+                map_prefix,
+                'ab_group')
     else:
-        print "Group [motivation] skipped"
-
-    if analysis.is_group(args, 'progress') and all(map(lambda x: not isinstance(x, str) or x.isdigit(), mapping.values())):
-        fig = plt.figure()
-        graph.plot_user_ratio(fig, data, 'ab_group', mapping, session_numbers=[1, 2])
-        fig.suptitle('AB testing: Users with at least the given number of sessions')
-        analysis.savefig(args, fig, 'users_with_n_sessions', prefix=prefix)
-
-        fig = plt.figure()
-        graph.plot_user_ratio(fig, data, 'ab_group', mapping, answer_numbers_min=[20, 30, 50])
-        fig.suptitle('AB testing: Users with at least the given number of answers')
-        analysis.savefig(args, fig, 'users_with_n_answers', prefix=prefix)
-        print "Group [progress] processed"
-    else:
-        print "Group [progress] skipped"
-
-    if analysis.is_group(args, 'difference'):
-        fig = plt.figure()
-        graph.boxplot_number_of_options(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: Number of options')
-        analysis.savefig(args, fig, 'number_of_options', prefix=prefix)
-
-        fig = plt.figure()
-        graph.boxplot_time_gap(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: Average Time Gap per User')
-        analysis.savefig(args, fig, 'time_gap', prefix=prefix)
-
-        fig = plt.figure()
-        graph.hist_answers_per_place_user(fig, data, 'ab_group', mapping)
-        fig.suptitle('AB testing: Number of Answer per Place and User')
-        analysis.savefig(args, fig, 'answers_per_place_user', prefix=prefix)
-        print "Group [difference] processed"
-    else:
-        print "Group [difference] skipped"
-
-    if analysis.is_group(args, 'text'):
-        with open(analysis.get_destination(args, prefix) + '/output.txt', 'w') as f:
-            textstats.answers_per_user(f, data, 'ab_group', mapping)
-            textstats.answers_per_user_pvalues(f, data, 'ab_group', mapping)
-            textstats.user_ratio(f, data, 'ab_group', mapping)
-        print "Group [text] processed"
-    else:
-        print "Group [text] skipped"
+        print "# Processing AB group"
+        map_graphs(args, data, mapping, prefix, '', 'ab_group')
 
 
 if __name__ == "__main__":
